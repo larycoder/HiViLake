3bef6319736f9087f8ad6ce714cca6d70598c61b
larycoder <lenhuchuhiep99@gmail.com>
2020-09-02 21:57:20 +0000

    LH    : init hadoop FilesystemWrapper implementation

--- storage/hadoop-storage/src/main/java/com/usth/hieplnc/storage/hadoop/Storage.java
+++ storage/hadoop-storage/src/main/java/com/usth/hieplnc/storage/hadoop/Storage.java
@@ -8,16 +8,25 @@ package com.usth.hieplnc.storage.hadoop;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.*;
+import org.json.simple.JSONObject;
 
 import java.io.IOException;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Iterator;
 
 // Storage wrapper interface
 import com.usth.hieplnc.storage.api.StorageWrapper;
-import com.usth.hieplnc.storage.api.Filesystem;
+import com.usth.hieplnc.storage.api.filesystem.*;
+import com.usth.hieplnc.storage.api.filesystem.model.*;
 
-public class Storage implements StorageWrapper{
+/**
+ * WRANNING:
+ * - Missing Exception throwing in close method
+ *
+ */
+
+public class Storage implements FilesystemWrapper{
 // variable
     
     private final Configuration conf;
@@ -55,11 +64,36 @@ public class Storage implements StorageWrapper{
         }
     }
 
+    @Override
+    public void createPath(String path, PathType type){
+        ;
+    }
+    
+    @Override
+    public void deletePath(String path, SWOption option){
+        ;
+    }
+    
+    @Override
+    public SWFile openFile(String path){
+        return null;
+    }
+    
+    @Override
+    public SWFilestatus getStatus(String path){
+        return null;
+    }
+    
+    @Override
+    public Iterator<SWFilestatus> searchFile(JSONObject pattern){
+        return null;
+    }
+
     public static void main(String args[]) throws IOException{
 // test StorageWrapper
         Configuration hadoopConf = new Configuration();
         hadoopConf.set("fs.defaultFS", "hdfs://localhost:9000");
-        StorageWrapper myStore = new Storage(hadoopConf);
+        StorageWrapper myStore = (StorageWrapper) new Storage(hadoopConf);
 // type of storage supported by this class
         for(String type: myStore.support()){
             System.out.println(type);
